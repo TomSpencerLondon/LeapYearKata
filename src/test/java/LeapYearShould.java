f45401d48571ae0eab9b27b3a79fd08bf5340cb7
Sebastian Kuliński <morinslash@Sebastians-MacBook-Pro.local>
2020-03-02 14:24:51 +0100

    red part of test for not divisble by number

--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -1,9 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LeapYearShould {
 
     @Test
     void return_false_if_not_divisible_by_4() {
-
+        Assertions.assertFalse(LeapYear.isLeap(405));
     }
 }
