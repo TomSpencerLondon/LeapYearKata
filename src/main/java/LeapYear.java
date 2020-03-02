3cae5d54f0c0c19dfc2dcf37c4b01eefe00dc7b2
Tom Spencer <tom@homeflow.co.uk>
2020-03-02 14:06:25 +0000

    Extract booleans to isDivisibleBy method

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,12 +1,16 @@
 public class LeapYear {
 
     public static boolean check(int year){
-        if ((year % 100 == 0) && !(year % 400 == 0)){
+        if (isDivisibleBy(year, 100) && !(year % 400 == 0)){
             return false;
         }
         if (year % 4 == 0){
             return true;
         }
         return false;
+    }
+
+    private static boolean isDivisibleBy(int year, int factor) {
+        return year % factor == 0;
     }
 }
